f88d6f6ad3e868adbe1f8f6940e35e32e5935b1c
Alec Lepe <lepealec@berkeley.edu>
2015-02-25 08:37:32 -0800

    hw4

--- assignments/hw4/hw4.r
+++ assignments/hw4/hw4.r
@@ -14,70 +14,77 @@
 #   element of <data.list>
 
 listLengths <- function(data.list) {
-
-    # your code here
-
+  return (sapply(data.list,length))
 }
 
 #### Function 2
 #### Implement the function "powers"
 
 # Input variable :
-# <x> : a numeric vector of length n
+# <x> : a numeric vector of length n #length of vector is n
 # <k> : an integer
 
 # Output variable
-# <x.powers> : A matrix of size [n x k] where the first column is x, the second column x^2, the third column x^4, etc.
+# <x.powers> : A matrix of size [n x k] or [length(x),k] where the first column is x, the second column x^2, the third column x^4, etc.
 #              the column names should be : "x", "x^2", "x^3" etc.
 
 powers <- function(x, k){
-
+    power_matrix=matrix(nrow=length(x),ncol=k)  ; i=1
+    while (i<=k){
+      power_matrix[,i]=x**i
+      i=i+1
+    }
+    if (k==1){
+      dimnames(power_matrix)=list(seq(1,length(x)),"x")
+    }else{
+      dimnames(power_matrix)=list(seq(1,length(x)),c("x",paste("x^",seq(2,k),sep="")))
+    }
+    return (power_matrix)
 }
 
- 
 #### Function #3
 #### Implement the function "recipeConversion"
-
 # Input variable:
 # <recipe> : A data frame with three columns named "amount", "unit" and "ingredient"
-
 # Output variable:
 # <recipe.metric> : A data frame with three columns where cups have been converted to ml and ounces to grams.
 #                   the number in "amount" should be updated, and the entry in "unit" changed
 #                   both ml and gr should be rounded to the nearest multiple of 5,
 #                   e.g. a row that read : [2 cups flour] should now be [475 ml flour]
 #                   Note, if the "unit" is neither "cup"/"cups" nor "oz" the row should not be changed
-
 # The conversion constants are: 
 # 1 cup = 236.6 ml and 1 oz = 28.3 gr
 # Please use these exact numbers, do not add decimal places.
-
 # "unit" can take any of a number of values but you need to find the rows where
 # "unit" is : "cup", "cups" or "oz"
-
 # Note: to find a match in "unit" you have a few different options, you can go row by row
 # and check if the unit is equal to cup/cups/oz using the "==" operator, you can use the
 # match() or %in% operators or finally you can look at the function grep(). 
-
 # If the column names of the input data frame are not "amount", "unit" and "ingredient" the
 # function should stop and print out an error message
 
 # Put your code here
-recipeConversion <- function(recipe){
-
+recipeConversion = function(recipe){
+  if (colnames(recipe)[1]=="amount" & colnames(recipe)[2]=="unit" & colnames(recipe)[3]=="ingredient"){
+    cup_index=recipe$unit%in%c("cups","cup")
+    recipe$unit[cup_index]="ml"
+    recipe$amount[cup_index]=round*((recipe$amount[cup_index]*236.6)/5)*5
+    cup_index=recipe$unit%in%("oz") 
+    recipe$unit[oz_index]="gr"
+    recipe$amount[oz_index]=round*((recipe$amount[oz_index]*28.3)/5)*5
+    return (recipe)    
+  } else {
+    stop("Invalid column names!")
+  }
 }
-
 
 #### Function #4a
 # Implement the function "bootstrapVarEst"
-
 # Input variable:
 # <x> : data vector
 # <B> : the number of boostrap iterations
-
 # Output variable:
 # <boot.sigma2.est> : Bootstrap estimate for the variance of the sample mean (see lecture notes)
-
 # The bootstrap is a resampling method used here to estimate the variance of the sample mean.
 # Given a sample, X_1, X_2, ..., X_n of observations we want to estimate the mean (expected value)
 # of the distrubution of the random variables X, and we want an estimate of the variance of that 
@@ -88,20 +95,22 @@ recipeConversion <- function(recipe){
 #    i.e. we draw n values at random *with replacement* from the observations X_1, ..., X_n. (use sample())
 # -- Calculate, and store, the mean of this bootstrap sample, call that mu_i (i in 1:B)
 # -- The bootstrap variance is the sample variance of mu_1, mu_2, ..., mu_B
-
 bootstrapVarEst <- function(x, B){
-
+  i=1 ; mu_i=seq(1,B)
+  while (i<=B){
+    mu_i[i]=mean(sample(x,replace=TRUE))
+    i=i+1
+  }
+  boot.sigma2.est=var(mu_i)
+  return (boot.sigma2.est)
 }
 
 #### Function #4b
 #### Implement the function "jackknifeVarEst"
-
 # Input variable:
 # <x> : data vector
-
 # Output variable:
 # <jack.sigma2.est> : Jackknife estimate for the variance of the sample mean (see lecture notes)
-
 # The jackknife is a resampling method used here to estimate the variance of the sample mean.
 # Given a sample, X_1, X_2, ..., X_n of observations we want to estimate the mean (expected value)
 # of the distrubution of the random variables X, and we want an estimate of the variance of that 
@@ -110,25 +119,30 @@ bootstrapVarEst <- function(x, B){
 # -- For each i in 1:n we omit the i-th observation (i.e. take the sample X_1, ..., X_[i-1], X_[i+1], ..., X_n)
 #     for this reduced sample calculate the sample mean (get mu_1, mu_2, ..., mu_n)
 # -- The jackknife variance is the sample variance of mu_1, mu_2, ..., mu_n
-
-jackknifeVarEst <- fuction(x){
-
-}
-
+jackknifeVarEst =function(x){
+  n=length(x)       ;     i=1
+  total_sum=sum(x)  ;     mu_i=c(seq(1,n))
+  while (i<=n){
+    mu_i[i]=(total_sum-x[i])/(n-1)
+    i=i+1
+  }
+  jack.sigma2.est=var(mu_i)
+  return (jack.sigma2.est)
+} 
 #### Function #4c
 #### Implement the function "samplingVarEst"
-
 # Input variables:
 # <x> : data vector
 # <type> : string that takes the values "bootstrap" or "jackknife", the default should be bootstrap.
-
 # Output variable:
 # <sampling.sigma.est> : The bootstrap estimate if type="bootstrap" and the jackknife estimate if type="jackknife"
-
 # Note: this function calls the previous two functions.
-
-samplingVarEst <- function(  ){
-
-}
-
-
+samplingVarEst <- function(x,type="bootstrap",B=1000){
+  if (type=="bootstrap"){
+    return(bootstrapVarEst(x,B))
+    }else if (type=="jackknife"){
+      return (jackknifeVarEst(x))
+      }else{
+        stop("type not 'bootstrap' or 'jacnkniffe'")
+      }
+  }
